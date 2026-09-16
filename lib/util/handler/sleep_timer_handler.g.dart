e04c0e190debaa312c388d4a088c3108a20f4c4a
Kyomorie <246901271+Kyomorie@users.noreply.github.com>
2026-09-16 09:46:53 +0200

    chore: regenerate sleep timer provider hash

--- lib/util/handler/sleep_timer_handler.g.dart
+++ lib/util/handler/sleep_timer_handler.g.dart
@@ -37,7 +37,7 @@ final class SleepTimerHandlerProvider extends $NotifierProvider<SleepTimerHandle
   }
 }
 
-String _$sleepTimerHandlerHash() => r'7b2874f7cb1c1c79e4b268b846aafb0ae2867606';
+String _$sleepTimerHandlerHash() => r'a82c5edbe33b88c0d07c60d4a699ebd73873620e';
 
 abstract class _$SleepTimerHandler extends $Notifier<SleepTimerData> {
   SleepTimerData build();
