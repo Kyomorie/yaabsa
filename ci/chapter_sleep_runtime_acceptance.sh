581c8d5df71af0b116fbfe3adb5fdcf6421c9eec
Kyomorie <246901271+Kyomorie@users.noreply.github.com>
2026-09-18 00:07:26 +0200

    ci: align scenario emulator renderer with setup

--- ci/chapter_sleep_runtime_acceptance.sh
+++ ci/chapter_sleep_runtime_acceptance.sh
@@ -60,7 +60,7 @@ nohup "$emulator" \
   -no-metrics \
   -memory 2048 \
   -cores 2 \
-  -gpu software \
+  -gpu swiftshader_indirect \
   -accel "$accel" \
   </dev/null > emulator.log 2>&1 &
 emulator_pid=$!
